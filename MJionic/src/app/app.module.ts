f4308ef6a2a051e6f282d77a63fd0e26a3d18224
f.Auxietre <f.Auxietre@gmail.com>
2020-01-29 10:17:17 +0100

    generateur de de

--- MJionic/src/app/app.module.ts
+++ MJionic/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import {GeneratorPage} from "../pages/generator/generator";
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    GeneratorPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +31,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    GeneratorPage
+
   ],
   providers: [
     StatusBar,
